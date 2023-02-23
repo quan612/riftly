88d24f47578d11e031777119b7451b501764474c
Quan Huynh <quan612@yahoo.com>
2023-02-23 09:07:31 -0500

    redesign user stats search

--- pages/api/admin/user-stats/index.js
+++ pages/api/admin/user-stats/index.js
@@ -8,40 +8,46 @@ const adminUserStatsAPI = async (req, res) => {
     const { method } = req;
 
     if (method === "POST") {
-        const currentPage = req.query?.page;
-        let searchRes = {}, userCondition = {};
+        try {
+            const currentPage = req.query?.page;
+            let searchRes = {}, userCondition = {};
 
-        const userCount = await prisma.whiteList.count();
+            const userCount = await prisma.whiteList.count();
 
-        const users = await prisma.whiteList.findMany({
-            where: userCondition,
-            skip: currentPage * skipNumber,
-            take: skipNumber,
-            orderBy: [
-                {
-                    createdAt: "asc",
+            const users = await prisma.whiteList.findMany({
+                where: userCondition,
+                skip: currentPage * skipNumber,
+                take: skipNumber,
+                orderBy: [
+                    {
+                        createdAt: "asc",
+                    },
+                ],
+                select: {
+                    wallet: true,
+                    twitterUserName: true,
+                    discordUserDiscriminator: true,
+                    whiteListUserData: true,
+                    userId: true
                 },
-            ],
-            select: {
-                wallet: true,
-                twitterUserName: true,
-                discordUserDiscriminator: true,
-                whiteListUserData: true,
-                userId: true
-            },
-        });
+            });
 
-        searchRes.userCount = userCount;
-        searchRes.users = users;
-        if (currentPage * skipNumber >= userCount) {
-            searchRes.shouldContinue = false;
-        } else {
-            searchRes.shouldContinue = true;
+            searchRes.userCount = userCount;
+            searchRes.users = users;
+            if (currentPage * skipNumber >= userCount) {
+                searchRes.shouldContinue = false;
+            } else {
+                searchRes.shouldContinue = true;
+            }
+
+            // searchRes.shouldContinue = false;
+            res.setHeader('Cache-Control', 'max-age=0, s-maxage=18000, stale-while-revalidate');
+            return res.status(200).json(searchRes);
+        } catch (error) {
+            console.log(error)
+            return res.status(200).json({ isError: true, message: error.message });
         }
 
-        // searchRes.shouldContinue = false;
-        res.setHeader('Cache-Control', 'max-age=0, s-maxage=18000, stale-while-revalidate');
-        return res.status(200).json(searchRes);
     }
     throw new ApiError(400, `Method ${req.method} Not Allowed`)
 };
