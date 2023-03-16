2b2e6fe5c2b98e0fea97e766d64a08393e2b0067
Quan Huynh <quan612@yahoo.com>
2023-03-16 15:06:25 -0400

    refactoring and fix bugs

--- src/components/end-user/shared/UnstoppableQuestModal.jsx
+++ src/components/end-user/shared/UnstoppableQuestModal.jsx
@@ -69,6 +69,7 @@ const UnstoppableQuestModal = ({ isOpen, onClose, currentQuest }) => {
       uauthUser,
     }
     let res = await submitQuest(payload)
+    console.log(res)
     if (res?.isError) {
       errorSet(res.message)
     } else {
